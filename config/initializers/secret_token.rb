10f4a07b4eb8e7eeffaf3f0d7b9413de5c905895
Najeeb Khan <nk21@ymail.com>
2015-02-04 11:17:17 -0500

    Created Server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OneStripe::Application.config.secret_key_base = '4d429116a59b4a5458df10222ae87c4b4deb4ebba967d83622b662e60adff9be7d913c24c25c834c91c4840c7855bfbd3209b8c68c1d84cabe478b2b303b9add'
